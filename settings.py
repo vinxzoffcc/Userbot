dc86f3aa629b126d25a796326774fad8362b6c0d
VinxzOffcial <vinzzmark3@gmail.com>
2025-11-01 13:41:09 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+# === TELEGRAM OWNER ===
+OWNER_ID = 7868229705
+PP = "https://files.catbox.moe/zzx0s5.jpg"
+BOT_TOKEN = "8193607838:AAG3skaKfEuFNkA79s_HQyj-jFMrs15Gb7k"
+# === PTERODACTYL PANEL ===
+DOMAIN = "YourDomain"
+PLTA = "ptla_ZeiAUwnqme299jhutwvbKYpOS0AWDUORRKJQtG4qbdR"
+PLTC = "ptlc_YauSzZjWSSWJFFy8HWKvWaas961QbiQ46qFMcLGbSci"
+LOC = 1
+EGGS = 15
+# === TELEGRAM API (my.telegram.org) ===
+API_ID =  # ganti dengan api yang sesuai 
+API_HASH = "" # ----"-----
